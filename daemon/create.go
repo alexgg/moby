6a5ef2786ed5edd70b435b2df861131c88b27899
Petros Angelatos <petrosagg@gmail.com>
2017-07-27 13:38:23 -0700

    daemon: skip initLayer for bare runtime containers
    Containers that are meant to be booted from do not need the initLayer.
    
    The default init layer shadows /etc/resolv.conf and other files from the
    filesystem but this can cause problems if we're bootstrapping the
    system.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- daemon/create.go
+++ daemon/create.go
@@ -205,8 +205,12 @@ func (daemon *Daemon) create(opts createOpts) (retC *container.Container, retErr
 		}
 	}
 
+	initFunc := setupInitLayer(daemon.idMapping)
+	if opts.params.HostConfig.Runtime == "bare" {
+		initFunc = nil
+	}
 	// Set RWLayer for container after mount labels have been set
-	rwLayer, err := daemon.imageService.CreateLayer(ctr, setupInitLayer(daemon.idMapping))
+	rwLayer, err := daemon.imageService.CreateLayer(container, initFunc)
 	if err != nil {
 		return nil, errdefs.System(err)
 	}
