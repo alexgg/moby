theja0473/balena

mobynit: Add support to mount rootfs from a custom location
This patch adds support to pass an argument to mobynit which allows
mobynit to mount a rootfs from a custom path.

e.g. ./mobynit -sysroot /mnt/sysroot/inactive
will mount the rootfs partition from /mnt/sysroot/inactive and return
the destination path in stdout.

Signed-off-by: Zubair Lutfullah Kakakhel <zubair@resin.io>

cmd/mobynit/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,19 +23,10 @@ const (
 	PIVOT_PATH = "/mnt/sysroot/active"
 )
 
-func mountContainer(containerID, graphDriver string) string {
-	if err := os.MkdirAll("/dev/shm", os.ModePerm); err != nil {
-		log.Fatal("creating /dev/shm failed:", err)
-	}
-
-	if err := unix.Mount("shm", "/dev/shm", "tmpfs", 0, ""); err != nil {
-		log.Fatal("error mounting /dev/shm:", err)
-	}
-	defer unix.Unmount("/dev/shm", unix.MNT_DETACH)
-
+func mountContainer(layer_root, containerID, graphDriver string) string {
 	ls, err := layer.NewStoreFromOptions(layer.StoreOptions{
-		StorePath:                 LAYER_ROOT,
-		MetadataStorePathTemplate: filepath.Join(LAYER_ROOT, "image", "%s", "layerdb"),
+		StorePath:                 layer_root,
+		MetadataStorePathTemplate: filepath.Join(layer_root, "image", "%s", "layerdb"),
 		IDMappings:                &idtools.IDMappings{},
 		GraphDriver:               graphDriver,
 		OS:                        "linux",
@@ -56,6 +49,22 @@ func mountContainer(containerID, graphDriver string) string {
 	if err := unix.Mount("", newRootPath, "", unix.MS_REMOUNT, ""); err != nil {
 		log.Fatal("error remounting container as read/write:", err)
 	}
+
+	return newRootPath
+}
+
+func prepareForPivot(sysroot, containerID, graphDriver string) string {
+	if err := os.MkdirAll("/dev/shm", os.ModePerm); err != nil {
+		log.Fatal("creating /dev/shm failed:", err)
+	}
+
+	if err := unix.Mount("shm", "/dev/shm", "tmpfs", 0, ""); err != nil {
+		log.Fatal("error mounting /dev/shm:", err)
+	}
+	defer unix.Unmount("/dev/shm", unix.MNT_DETACH)
+
+	newRootPath := mountContainer(filepath.Join(sysroot, LAYER_ROOT), containerID, graphDriver)
+
 	defer unix.Mount("", newRootPath, "", unix.MS_REMOUNT|unix.MS_RDONLY, "")
 
 	if err := os.MkdirAll(filepath.Join(newRootPath, PIVOT_PATH), os.ModePerm); err != nil {
@@ -65,46 +74,68 @@ func mountContainer(containerID, graphDriver string) string {
 	return newRootPath
 }
 
-func main() {
-	// Any mounts done by initrd will be transfered in the new root
-	mounts, err := mount.GetMounts()
-
-	rawGraphDriver, err := ioutil.ReadFile("/current/boot/storage-driver")
+func getStorageDriverAndContainerID(sysroot string) (string, string) {
+	rawGraphDriver, err := ioutil.ReadFile(filepath.Join(sysroot, "/current/boot/storage-driver"))
 	if err != nil {
 		log.Fatal("could not get storage driver:", err)
 	}
 	graphDriver := strings.TrimSpace(string(rawGraphDriver))
 
-	current, err := os.Readlink("/current")
+	current, err := os.Readlink(filepath.Join(sysroot, "/current"))
 	if err != nil {
 		log.Fatal("could not get container ID:", err)
 	}
 	containerID := filepath.Base(current)
 
-	if err := unix.Mount("", "/", "", unix.MS_REMOUNT, ""); err != nil {
-		log.Fatal("error remounting root as read/write:", err)
+	return graphDriver, containerID
+}
+
+func main() {
+	sysrootPtr := flag.String("sysroot", "", "root of partition e.g. /mnt/sysroot/inactive. Mount destination is returned in stdout")
+	flag.Parse()
+
+	// Any mounts done by initrd will be transfered in the new root
+	mounts, err := mount.GetMounts()
+	if err != nil {
+		log.Fatal("could not get mounts:", err)
 	}
 
-	newRoot := mountContainer(containerID, graphDriver)
+	var graphDriver, containerID string
 
-	for _, mount := range mounts {
-		if mount.Mountpoint == "/" {
-			continue
+	// If a custom sysroot is passed, use it instead of LAYER_ROOT
+	if *sysrootPtr != "" {
+		graphDriver, containerID = getStorageDriverAndContainerID(*sysrootPtr)
+		newRootPath := mountContainer(filepath.Join(*sysrootPtr, LAYER_ROOT), containerID, graphDriver)
+		fmt.Print(newRootPath)
+
+	} else {
+		graphDriver, containerID = getStorageDriverAndContainerID("")
+
+		if err := unix.Mount("", "/", "", unix.MS_REMOUNT, ""); err != nil {
+			log.Fatal("error remounting root as read/write:", err)
 		}
-		if err := unix.Mount(mount.Mountpoint, filepath.Join(newRoot, mount.Mountpoint), "", unix.MS_MOVE, ""); err != nil {
-			log.Println("could not move mountpoint:", mount.Mountpoint, err)
+
+		newRoot := prepareForPivot("", containerID, graphDriver)
+
+		for _, mount := range mounts {
+			if mount.Mountpoint == "/" {
+				continue
+			}
+			if err := unix.Mount(mount.Mountpoint, filepath.Join(newRoot, mount.Mountpoint), "", unix.MS_MOVE, ""); err != nil {
+				log.Println("could not move mountpoint:", mount.Mountpoint, err)
+			}
 		}
-	}
 
-	if err := syscall.PivotRoot(newRoot, filepath.Join(newRoot, PIVOT_PATH)); err != nil {
-		log.Fatal("error while pivoting root:", err)
-	}
+		if err := syscall.PivotRoot(newRoot, filepath.Join(newRoot, PIVOT_PATH)); err != nil {
+			log.Fatal("error while pivoting root:", err)
+		}
 
-	if err := unix.Chdir("/"); err != nil {
-		log.Fatal(err)
-	}
+		if err := unix.Chdir("/"); err != nil {
+			log.Fatal(err)
+		}
 
-	if err := syscall.Exec("/sbin/init", os.Args, os.Environ()); err != nil {
-		log.Fatal("error executing init:", err)
+		if err := syscall.Exec("/sbin/init", os.Args, os.Environ()); err != nil {
+			log.Fatal("error executing init:", err)
+		}
 	}
 }

alexgg/moby

cmd/mobynit/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,19 +23,10 @@ const (
 	PIVOT_PATH = "/mnt/sysroot/active"
 )
 
-func mountContainer(containerID, graphDriver string) string {
-	if err := os.MkdirAll("/dev/shm", os.ModePerm); err != nil {
-		log.Fatal("creating /dev/shm failed:", err)
-	}
-
-	if err := unix.Mount("shm", "/dev/shm", "tmpfs", 0, ""); err != nil {
-		log.Fatal("error mounting /dev/shm:", err)
-	}
-	defer unix.Unmount("/dev/shm", unix.MNT_DETACH)
-
+func mountContainer(layer_root, containerID, graphDriver string) string {
 	ls, err := layer.NewStoreFromOptions(layer.StoreOptions{
-		Root:                      LAYER_ROOT,
-		MetadataStorePathTemplate: filepath.Join(LAYER_ROOT, "image", "%s", "layerdb"),
+		Root:                      layer_root,
+		MetadataStorePathTemplate: filepath.Join(layer_root, "image", "%s", "layerdb"),
 		IDMapping:                 &idtools.IdentityMapping{},
 		GraphDriver:               graphDriver,
 		OS:                        "linux",
@@ -56,6 +49,22 @@ func mountContainer(containerID, graphDriver string) string {
 	if err := unix.Mount("", newRootPath, "", unix.MS_REMOUNT, ""); err != nil {
 		log.Fatal("error remounting container as read/write:", err)
 	}
+
+	return newRootPath
+}
+
+func prepareForPivot(containerID, graphDriver string) string {
+	if err := os.MkdirAll("/dev/shm", os.ModePerm); err != nil {
+		log.Fatal("creating /dev/shm failed:", err)
+	}
+
+	if err := unix.Mount("shm", "/dev/shm", "tmpfs", 0, ""); err != nil {
+		log.Fatal("error mounting /dev/shm:", err)
+	}
+	defer unix.Unmount("/dev/shm", unix.MNT_DETACH)
+
+	newRootPath := mountContainer(filepath.Join("", LAYER_ROOT), containerID, graphDriver)
+
 	defer unix.Mount("", newRootPath, "", unix.MS_REMOUNT|unix.MS_RDONLY, "")
 
 	if err := os.MkdirAll(filepath.Join(newRootPath, PIVOT_PATH), os.ModePerm); err != nil {
@@ -65,46 +74,67 @@ func mountContainer(containerID, graphDriver string) string {
 	return newRootPath
 }
 
-func main() {
-	// Any mounts done by initrd will be transfered in the new root
-	mounts, err := mount.GetMounts(nil)
-
-	rawGraphDriver, err := ioutil.ReadFile("/current/boot/storage-driver")
+func getStorageDriverAndContainerID(sysroot string) (string, string) {
+	rawGraphDriver, err := ioutil.ReadFile(filepath.Join(sysroot, "/current/boot/storage-driver"))
 	if err != nil {
 		log.Fatal("could not get storage driver:", err)
 	}
 	graphDriver := strings.TrimSpace(string(rawGraphDriver))
 
-	current, err := os.Readlink("/current")
+	current, err := os.Readlink(filepath.Join(sysroot, "/current"))
 	if err != nil {
 		log.Fatal("could not get container ID:", err)
 	}
 	containerID := filepath.Base(current)
 
-	if err := unix.Mount("", "/", "", unix.MS_REMOUNT, ""); err != nil {
-		log.Fatal("error remounting root as read/write:", err)
+	return graphDriver, containerID
+}
+
+func main() {
+	sysrootPtr := flag.String("sysroot", "", "root of partition e.g. /mnt/sysroot/inactive. Mount destination is returned in stdout")
+	flag.Parse()
+
+	// Any mounts done by initrd will be transfered in the new root
+	mounts, err := mount.GetMounts(nil)
+	if err != nil {
+		log.Fatal("could not get mounts:", err)
 	}
 
-	newRoot := mountContainer(containerID, graphDriver)
+	var graphDriver, containerID string
 
-	for _, mount := range mounts {
-		if mount.Mountpoint == "/" {
-			continue
+	// If a custom sysroot is passed, use it instead of LAYER_ROOT
+	if *sysrootPtr != "" {
+		graphDriver, containerID = getStorageDriverAndContainerID(*sysrootPtr)
+		newRootPath := mountContainer(filepath.Join(*sysrootPtr, LAYER_ROOT), containerID, graphDriver)
+		fmt.Print(newRootPath)
+	} else {
+		graphDriver, containerID = getStorageDriverAndContainerID("")
+
+		if err := unix.Mount("", "/", "", unix.MS_REMOUNT, ""); err != nil {
+			log.Fatal("error remounting root as read/write:", err)
 		}
-		if err := unix.Mount(mount.Mountpoint, filepath.Join(newRoot, mount.Mountpoint), "", unix.MS_MOVE, ""); err != nil {
-			log.Println("could not move mountpoint:", mount.Mountpoint, err)
+
+		newRoot := prepareForPivot(containerID, graphDriver)
+
+		for _, mount := range mounts {
+			if mount.Mountpoint == "/" {
+				continue
+			}
+			if err := unix.Mount(mount.Mountpoint, filepath.Join(newRoot, mount.Mountpoint), "", unix.MS_MOVE, ""); err != nil {
+				log.Println("could not move mountpoint:", mount.Mountpoint, err)
+			}
 		}
-	}
 
-	if err := syscall.PivotRoot(newRoot, filepath.Join(newRoot, PIVOT_PATH)); err != nil {
-		log.Fatal("error while pivoting root:", err)
-	}
+		if err := syscall.PivotRoot(newRoot, filepath.Join(newRoot, PIVOT_PATH)); err != nil {
+			log.Fatal("error while pivoting root:", err)
+		}
 
-	if err := unix.Chdir("/"); err != nil {
-		log.Fatal(err)
-	}
+		if err := unix.Chdir("/"); err != nil {
+			log.Fatal(err)
+		}
 
-	if err := syscall.Exec("/sbin/init", os.Args, os.Environ()); err != nil {
-		log.Fatal("error executing init:", err)
+		if err := syscall.Exec("/sbin/init", os.Args, os.Environ()); err != nil {
+			log.Fatal("error executing init:", err)
+		}
 	}
 }