balena-os/balena-engine

Rename balena to balena-engine (executables) or balenaEngine (project)
Signed-off-by: Paulo Castro <paulo@resin.io>

daemon/config/config_test.go
@@ -73,7 +73,7 @@ func TestFindConfigurationConflictsWithNamedOptions(t *testing.T) {
 	var hosts []string
 	flags.VarP(opts.NewNamedListOptsRef("hosts", &hosts, opts.ValidateHost), "host", "H", "Daemon socket(s) to connect to")
 	assert.NoError(t, flags.Set("host", "tcp://127.0.0.1:4444"))
-	assert.NoError(t, flags.Set("host", "unix:///var/run/balena.sock"))
+	assert.NoError(t, flags.Set("host", "unix:///var/run/balena-engine.sock"))
 
 	testutil.ErrorContains(t, findConfigurationConflicts(config, flags), "hosts")
 }
@@ -181,12 +181,12 @@ func TestFindConfigurationConflictsWithMergedValues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	flags.Set("host", "unix:///var/run/balena.sock")
+	flags.Set("host", "unix:///var/run/balena-engine.sock")
 	err = findConfigurationConflicts(config, flags)
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
-	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena.sock], from file: tcp://127.0.0.1:2345)") {
+	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena-engine.sock], from file: tcp://127.0.0.1:2345)") {
 		t.Fatalf("expected hosts conflict, got %v", err)
 	}
 }

alexgg/moby

daemon/config/config_test.go
@@ -72,7 +72,7 @@ func TestFindConfigurationConflictsWithNamedOptions(t *testing.T) {
 	var hosts []string
 	flags.VarP(opts.NewNamedListOptsRef("hosts", &hosts, opts.ValidateHost), "host", "H", "Daemon socket(s) to connect to")
 	assert.Check(t, flags.Set("host", "tcp://127.0.0.1:4444"))
-	assert.Check(t, flags.Set("host", "unix:///var/run/balena.sock"))
+	assert.Check(t, flags.Set("host", "unix:///var/run/balena-engine.sock"))
 	assert.Check(t, is.ErrorContains(findConfigurationConflicts(config, flags), "hosts"))
 }
 
@@ -221,12 +221,12 @@ func TestFindConfigurationConflictsWithMergedValues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	flags.Set("host", "unix:///var/run/balena.sock")
+	flags.Set("host", "unix:///var/run/balena-engine.sock")
 	err = findConfigurationConflicts(config, flags)
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
-	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena.sock], from file: tcp://127.0.0.1:2345)") {
+	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena-engine.sock], from file: tcp://127.0.0.1:2345)") {
 		t.Fatalf("expected hosts conflict, got %v", err)
 	}
 }