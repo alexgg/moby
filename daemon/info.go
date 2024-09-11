balena-os/balena-engine

daemon: return engine name as part of the version information
This can be used by clients that know how to talk to the extended API
balaena provides, e.g creating deltas

Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

daemon/info.go
@@ -154,6 +154,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 // SystemVersion returns version information about the daemon.
 func (daemon *Daemon) SystemVersion() types.Version {
 	v := types.Version{
+		Engine:        "balaena",
 		Version:       dockerversion.Version,
 		GitCommit:     dockerversion.GitCommit,
 		MinAPIVersion: api.MinVersion,

alexgg/moby

daemon/info.go
@@ -114,6 +114,7 @@ func (daemon *Daemon) SystemVersion() types.Version {
 		},
 
 		// Populate deprecated fields for older clients
+		Engine:        "balena",
 		Version:       dockerversion.Version,
 		GitCommit:     dockerversion.GitCommit,
 		APIVersion:    api.DefaultVersion,