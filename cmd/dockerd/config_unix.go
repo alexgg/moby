f5e68e18a300a32bb18afa24bb1694d3ad023b44
Alex Gonzalez <alexg@balena.io>
2024-09-11 16:03:44 +0200

    [SQ] cmd/dockerd

--- cmd/dockerd/config_unix.go
+++ cmd/dockerd/config_unix.go
@@ -53,7 +53,7 @@ func installConfigFlags(conf *config.Config, flags *pflag.FlagSet) error {
 	flags.StringVar(&conf.BridgeConfig.UserlandProxyPath, "userland-proxy-path", defaultUserlandProxyPath, "Path to the userland proxy binary")
 	flags.StringVar(&conf.CgroupParent, "cgroup-parent", "", "Set parent cgroup for all containers")
 	flags.StringVar(&conf.RemappedRoot, "userns-remap", "", "User/Group setting for user namespaces")
-	flags.BoolVar(&conf.LiveRestoreEnabled, "live-restore", false, "Enable live restore of balenaEngine when containers are still running")
+	flags.BoolVar(&conf.LiveRestoreEnabled, "live-restore", false, "Enable live restore of balena-engine when containers are still running")
 	flags.IntVar(&conf.OOMScoreAdjust, "oom-score-adjust", 0, "Set the oom_score_adj for the daemon")
 	flags.BoolVar(&conf.Init, "init", false, "Run an init in the container to forward signals and reap processes")
 	flags.StringVar(&conf.InitPath, "init-path", "", "Path to the balena-engine-init binary")
