balena-os/balena-engine

fixed integration with balena
Signed-off-by: Yossi Eliaz <yossi@resin.io>

daemon/config/config_test.go
@@ -76,7 +76,7 @@ func TestFindConfigurationConflictsWithNamedOptions(t *testing.T) {
 	var hosts []string
 	flags.VarP(opts.NewNamedListOptsRef("hosts", &hosts, opts.ValidateHost), "host", "H", "Daemon socket(s) to connect to")
 	assert.NoError(t, flags.Set("host", "tcp://127.0.0.1:4444"))
-	assert.NoError(t, flags.Set("host", "unix:///var/run/docker.sock"))
+	assert.NoError(t, flags.Set("host", "unix:///var/run/balena.sock"))
 
 	testutil.ErrorContains(t, findConfigurationConflicts(config, flags), "hosts")
 }
@@ -184,12 +184,12 @@ func TestFindConfigurationConflictsWithMergedValues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	flags.Set("host", "unix:///var/run/docker.sock")
+	flags.Set("host", "unix:///var/run/balena.sock")
 	err = findConfigurationConflicts(config, flags)
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
-	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/docker.sock], from file: tcp://127.0.0.1:2345)") {
+	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena.sock], from file: tcp://127.0.0.1:2345)") {
 		t.Fatalf("expected hosts conflict, got %v", err)
 	}
 }

opts/hosts_test.go
@@ -57,7 +57,7 @@ func TestParseDockerDaemonHost(t *testing.T) {
 		"tcp:a.b.c.d/path":              "Invalid bind address format: tcp:a.b.c.d/path",
 		"udp://127.0.0.1":               "Invalid bind address format: udp://127.0.0.1",
 		"udp://127.0.0.1:2375":          "Invalid bind address format: udp://127.0.0.1:2375",
-		"tcp://unix:///run/docker.sock": "Invalid proto, expected tcp: unix:///run/docker.sock",
+		"tcp://unix:///run/balena.sock": "Invalid proto, expected tcp: unix:///run/balena.sock",
 		" tcp://:7777/path ":            "Invalid bind address format:  tcp://:7777/path ",
 		"":                              "Invalid bind address format: ",
 	}
@@ -74,7 +74,7 @@ func TestParseDockerDaemonHost(t *testing.T) {
 		"tcp://":                  DefaultTCPHost,
 		"tcp://:7777":             fmt.Sprintf("tcp://%s:7777", DefaultHTTPHost),
 		"tcp://:7777/path":        fmt.Sprintf("tcp://%s:7777/path", DefaultHTTPHost),
-		"unix:///run/docker.sock": "unix:///run/docker.sock",
+		"unix:///run/balena.sock": "unix:///run/balena.sock",
 		"unix://":                 "unix://" + DefaultUnixSocket,
 		"fd://":                   "fd://",
 		"fd://something":          "fd://something",
@@ -137,14 +137,14 @@ func TestParseTCP(t *testing.T) {
 }
 
 func TestParseInvalidUnixAddrInvalid(t *testing.T) {
-	if _, err := parseSimpleProtoAddr("unix", "tcp://127.0.0.1", "unix:///var/run/docker.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
+	if _, err := parseSimpleProtoAddr("unix", "tcp://127.0.0.1", "unix:///var/run/balena.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
 		t.Fatalf("Expected an error, got %v", err)
 	}
-	if _, err := parseSimpleProtoAddr("unix", "unix://tcp://127.0.0.1", "/var/run/docker.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
+	if _, err := parseSimpleProtoAddr("unix", "unix://tcp://127.0.0.1", "/var/run/balena.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
 		t.Fatalf("Expected an error, got %v", err)
 	}
-	if v, err := parseSimpleProtoAddr("unix", "", "/var/run/docker.sock"); err != nil || v != "unix:///var/run/docker.sock" {
-		t.Fatalf("Expected an %v, got %v", v, "unix:///var/run/docker.sock")
+	if v, err := parseSimpleProtoAddr("unix", "", "/var/run/balena.sock"); err != nil || v != "unix:///var/run/balena.sock" {
+		t.Fatalf("Expected an %v, got %v", v, "unix:///var/run/balena.sock")
 	}
 }
 

alexgg/moby

daemon/config/config_test.go
@@ -72,7 +72,7 @@ func TestFindConfigurationConflictsWithNamedOptions(t *testing.T) {
 	var hosts []string
 	flags.VarP(opts.NewNamedListOptsRef("hosts", &hosts, opts.ValidateHost), "host", "H", "Daemon socket(s) to connect to")
 	assert.Check(t, flags.Set("host", "tcp://127.0.0.1:4444"))
-	assert.Check(t, flags.Set("host", "unix:///var/run/docker.sock"))
+	assert.Check(t, flags.Set("host", "unix:///var/run/balena.sock"))
 	assert.Check(t, is.ErrorContains(findConfigurationConflicts(config, flags), "hosts"))
 }
 
@@ -221,12 +221,12 @@ func TestFindConfigurationConflictsWithMergedValues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	flags.Set("host", "unix:///var/run/docker.sock")
+	flags.Set("host", "unix:///var/run/balena.sock")
 	err = findConfigurationConflicts(config, flags)
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
-	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/docker.sock], from file: tcp://127.0.0.1:2345)") {
+	if !strings.Contains(err.Error(), "hosts: (from flag: [unix:///var/run/balena.sock], from file: tcp://127.0.0.1:2345)") {
 		t.Fatalf("expected hosts conflict, got %v", err)
 	}
 }

opts/hosts_test.go
@@ -57,7 +57,7 @@ func TestParseDockerDaemonHost(t *testing.T) {
 		"tcp:a.b.c.d/path":              "",
 		"udp://127.0.0.1":               "Invalid bind address format: udp://127.0.0.1",
 		"udp://127.0.0.1:2375":          "Invalid bind address format: udp://127.0.0.1:2375",
-		"tcp://unix:///run/docker.sock": "Invalid proto, expected tcp: unix:///run/docker.sock",
+		"tcp://unix:///run/balena.sock": "Invalid proto, expected tcp: unix:///run/balena.sock",
 		" tcp://:7777/path ":            "Invalid bind address format:  tcp://:7777/path ",
 		"":                              "Invalid bind address format: ",
 	}
@@ -74,7 +74,7 @@ func TestParseDockerDaemonHost(t *testing.T) {
 		"tcp://":                      DefaultTCPHost,
 		"tcp://:7777":                 fmt.Sprintf("tcp://%s:7777", DefaultHTTPHost),
 		"tcp://:7777/path":            fmt.Sprintf("tcp://%s:7777/path", DefaultHTTPHost),
-		"unix:///run/docker.sock":     "unix:///run/docker.sock",
+		"unix:///run/docker.sock":     "unix:///run/balena.sock",
 		"unix://":                     "unix://" + DefaultUnixSocket,
 		"fd://":                       "fd://",
 		"fd://something":              "fd://something",
@@ -137,14 +137,14 @@ func TestParseTCP(t *testing.T) {
 }
 
 func TestParseInvalidUnixAddrInvalid(t *testing.T) {
-	if _, err := parseSimpleProtoAddr("unix", "tcp://127.0.0.1", "unix:///var/run/docker.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
+	if _, err := parseSimpleProtoAddr("unix", "tcp://127.0.0.1", "unix:///var/run/balena.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
 		t.Fatalf("Expected an error, got %v", err)
 	}
-	if _, err := parseSimpleProtoAddr("unix", "unix://tcp://127.0.0.1", "/var/run/docker.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
+	if _, err := parseSimpleProtoAddr("unix", "unix://tcp://127.0.0.1", "/var/run/balena.sock"); err == nil || err.Error() != "Invalid proto, expected unix: tcp://127.0.0.1" {
 		t.Fatalf("Expected an error, got %v", err)
 	}
-	if v, err := parseSimpleProtoAddr("unix", "", "/var/run/docker.sock"); err != nil || v != "unix:///var/run/docker.sock" {
-		t.Fatalf("Expected an %v, got %v", v, "unix:///var/run/docker.sock")
+	if v, err := parseSimpleProtoAddr("unix", "", "/var/run/balena.sock"); err != nil || v != "unix:///var/run/balena.sock" {
+		t.Fatalf("Expected an %v, got %v", v, "unix:///var/run/balena.sock")
 	}
 }
 